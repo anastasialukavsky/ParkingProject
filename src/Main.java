03a741ef687d64ff0ad1d9d801469306773bf640
ruralFluffyDill <stacylukavsky@gmail.com>
2023-10-31 22:34:53 -0400

    test file

---
+++ src/Main.java
@@ -0,0 +1,102 @@
+// import java.util.Arrays;
+// import java.util.Date;
+// import java.util.Scanner;
+
+// public class Main {
+
+//   public static void main(String[] args) {
+
+//     Scanner scanner = new Scanner(System.in);
+//     System.out.print("principal: ");
+//     int principal = Integer.parseInt(scanner.nextLine().trim());
+//     System.out.println("yearly interest");
+
+
+//    int res= Math.max(1,7);
+// System.out.println(res);
+    // int x= 1;
+    // ++x;
+    // System.out.println(x);
+    // byte  age = 50;
+    // long viewsCount = 3_123_456_789L;
+    // float price = 10.99F;
+    // char letter = 'a';
+    // boolean isEligible = true;
+    // Date now  = new Date();
+    // System.out.println("now");
+
+    // System.out.println(now);
+    // byte x = 1;
+    // byte y =2;
+    // x=2;
+    // System.out.println(y);
+
+    // Point point = new Point(x:1, y:2);
+    // int [] numbers = {7,9,1, 2, 4, 5 ,6 ,8};
+    // Arrays.sort(numbers);
+    // System.out.println(numbers);
+
+    // int [][] numbers = {{1, 2 , 3}, {4, 5, 6}};
+    // numbers[0][0] = 1;
+    // System.out.println(Arrays.deepToString(numbers));
+
+    // final float PI = 3.14f;
+
+    // double result = (double)10 / (double)3;
+    // System.out.println(result);
+
+
+
+  // public class Main {
+  //   public static void main (String[] args) {
+  //     int[] b = {1, 2,3, 4, 5};
+  //     for (int i = b.length -1; i>=0; i--) {
+  //       System.out.println(b[i]);
+  //     }
+  //   }
+  // }
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//       Scanner scanner = new Scanner(System.in);
+//       String[] quizQuestions = {
+//         "Math is annoying",
+//         "I need a new computer.",
+//         "Java is good",
+//         "My cat is healthy",
+//         "its nice out",
+//       };
+
+//       boolean[] quizAns = {
+//         true,
+//         true,
+//         true,
+//         true,
+//         false
+//       };
+
+//       boolean[] userAns = new boolean[quizQuestions.length];
+
+//       System.out.println("Quiz");
+//  int userScore = 0;
+//       for (int i = 0; i < quizQuestions.length; i++) {
+//         System.out.format("Question %d: %s. True or false?", i+1, quizQuestions[i]);
+
+//         userAns[i] = scanner.nextBoolean();
+
+//         if(userAns[i] == quizAns[i]) {
+//           userScore++;
+//         }
+
+
+//       }
+
+//       System.out.format("your score is %d out of %d (%.1f%)", userScore, quizQuestions.length, (float) userScore / (float) quizQuestions.length);
+
+
+
+    }
+   
+  }
